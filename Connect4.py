98a170ebcb5df7192525c758088c1add6df6226b
HariJones97 <61684178+HariJones97@users.noreply.github.com>
2020-07-03 18:08:24 +0100

    Added win condition boolean check

--- Connect4.py
+++ Connect4.py
@@ -7,7 +7,8 @@ class Connect4():
         self.board = np.zeros(width*height).reshape(height,width )
         self.width = width  - 1
         self.height = height - 1
-        self.counter = [0] * 7
+        self.counter = [0] * width
+        self.gamewon = False
         print (self.counter)
         print (self.board)
 
@@ -32,17 +33,35 @@ class Connect4():
 
     def play(self):
         turncount = 0
-        while turncount <= 5:
+        maxturns = (self.width+1)*(self.height+1)
+        while self.gamewon == False and turncount < maxturns:
             if turncount % 2 == 0:
                 game.player1turn()
             else:
                 game.player2turn()
             turncount+=1
+            self.wincheck(self.board)
 
+    def wincheck(self,board):
+        y,x = np.where(board==1)
+        
+        #flat
+        grid_up = board[:-1,:]
+        grid_down = board[1:,:]
+        grid_left = board[:,:-1]
+        grid_right = board[:,1:]
+        #diagonal
+        grid_upleft = board[:-1,:-1]
+        grid_upright = board[:-1,1:]
+        grid_downleft = board[1:,:-1]
+        grid_downright = board[1:,1:]
 
+        print(any(1 in t for t in grid_up[grid_down==1])
+        #if grid_up[grid_up[grid_down==1]==1].any():
+        #    print('bruh')
 
-
-
+        #for i,j in zip(y,x):
+            #print(i,j)
 
 game = Connect4('teamEugene', 'James')
 game.play()
